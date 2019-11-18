bc686456ba82f203d4684430112a62413f389b1d
ddominguez <davdomin@gmail.com>
2019-11-18 18:09:32 -0500

    mterializecss

--- resources/views/principal.php
+++ resources/views/principal.php
@@ -6,54 +6,56 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
-    <title>Collapsible sidebar using Bootstrap 4</title>
+    <title>Materlizecss</title>
 
     <!-- Bootstrap CSS CDN -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
-    <!-- Our Custom CSS -->
-    <link rel="stylesheet" href="css/menu.css">
+    <!-- Compiled and minified CSS -->
 
-    <!-- Font Awesome JS -->
-    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/solid.js" integrity="sha384-tzzSw1/Vo+0N5UhStP3bvwWPq+uvzCMfrN1fEFe+xBmv1C/AtVX5K0uZtmcHitFZ" crossorigin="anonymous"></script>
-    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/fontawesome.js" integrity="sha384-6OIrr52G08NpOFSZdxxz1xdNSndlD4vdcf/q2myIUVO0VsqaGHJsB0RaBE01VTOY" crossorigin="anonymous"></script>
+
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <!--Import materialize.css-->
+  <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+<script src="js/materialize.min.js"></script>
+  <!--Let browser know website is optimized for mobile-->
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  <link rel="stylesheet" href="css/menu.css">
 </head>
+
+
+
+</head>
+
+
 
 <body>
 
-  <div class="container">
+  <ul class="collapsible">    
+     <li>
+       <div class="collapsible-header"><i class="material-icons">filter_drama</i>First</div>
+     </li>
+     <li>
+       <div class="collapsible-header"><i class="material-icons">place</i>Second</div>
+     </li>
+     <li>
+       <div class="collapsible-header"><i class="material-icons">whatshot</i>Third</div>
+     </li>
+   </ul>
 
-  	<div class="row">
-  		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-  			<div class="page-header">
-  				<h2>Side Bar</h2>
-  			</div>
-  		</div>
-  	</div>
+   <script>
+   document.addEventListener('DOMContentLoaded', function() {
+       var elems = document.querySelectorAll('.collapsible');
+       var instances = M.Collapsible.init(elems, options);
+     });
 
-  	<div class="row">
+     // Or with jQuery
 
-
-  		<nav id="column_left">
-        <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
-  		<ul class="nav nav-list">
-        <?php=menu;?>
-  		</ul>
-
-  		</nav>
-
-  		</div>
-
-  		<div class="col-lg-9 col-md-9 col-sm-12 col-xs-12">
-
-
-  		</div>
-
-  	</div>
-
-  </div>
-
-
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+     $(document).ready(function(){
+       $('.collapsible').collapsible();
+     });
+              </script>
 </body>
 
 </html>
